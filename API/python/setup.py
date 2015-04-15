20228f78bf6ae5a271019a140d9c67ba2e9f2084
Jason Owens <jason.l.owens.civ@mail.mil>
2015-04-15 11:29:00 -0400

    modified python setup to link against local torch compilation.

--- API/python/setup.py
+++ API/python/setup.py
@@ -4,11 +4,12 @@ import os.path as path
 
 module1 = Extension("overfeat",
                     include_dirs = ['../../src', numpy.get_include()],
-                    library_dirs = ['../../src'],
+                    library_dirs = ['/usr/local/src/torch/install/lib','../../src','/opt/OpenBLAS/lib'],
                     libraries = ['TH', 'overfeat', 'openblas'],
                     sources = ['overfeatmodule.cpp'],
                     extra_compile_args=['-fopenmp'],
-                    extra_link_args=['-lgomp', '-lTH', '-L%s'%(path.abspath('../../src/libTH.a'))])
+                    #extra_link_args=['-lgomp', '-lTH', '-L%s'%(path.abspath('../../src/libTH.a'))])
+		    extra_link_args=['-lgomp'])
 
 setup(name = 'overfeat',
       version = '1.0',
